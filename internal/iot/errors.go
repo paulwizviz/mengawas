c9664e15c1010a72d05490722c5ee4adc8e1fa02
Paul Sitoh <paulwizviz@gmail.com>
2025-02-26 15:29:59 +0000

    Add iot package
    - Include error constants
    - Add temperature sub package
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

---
+++ internal/iot/errors.go
@@ -0,0 +1,9 @@
+package iot
+
+import "errors"
+
+var (
+	ErrNewMeasure         = errors.New("unable to instantiate new measurement")
+	ErrNotUTC             = errors.New("not valud UTC")
+	ErrInvalidMeasureCBOR = errors.New("invalid measurement cbor")
+)
